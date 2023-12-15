fa72722787df5e7d9c9ec1168506efee3e9affa2
Daniël Spoelder <daniel.spoelder@vanhalteren.com>
2023-12-15 14:17:38 +0100

    modified:   Data/Pizza.cs 	modified:   Data/PizzaService.cs 	modified:   Pages/PizzaPage.razor 	modified:   Program.cs

--- Data/Pizza.cs
+++ Data/Pizza.cs
@@ -1,7 +1,18 @@
 namespace BlazingPizza.Data
 {
+    // Pizza Item from Database
     public class Pizza
     {
-        
+        public int PizzaID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool Vegetarian { get; set; }
+
+        public bool Vegan { get; set; }
     }
 }
--- Data/PizzaService.cs
+++ Data/PizzaService.cs
@@ -1,7 +1,19 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualBasic;
+
 namespace BlazingPizza.Data
 {
+    //  Get Pizza Data
     public class PizzaService
     {
-        
+        public async Task<Pizza[]> GetPizzasAsync()
+        {
+            return await Task.Run( () => new Pizza[] {
+                new() { PizzaID = 1, Name = "Pizza 1", Description = "First Pizza in List", Price = 1.99M },
+                new() { PizzaID = 2, Name = "Pizza 2", Description = "Second Pizza in List", Price = 2.99M, Vegan = true },
+                new() { PizzaID = 3, Name = "Pizza 3", Description = "Third Pizza in List", Price = 3.99M, Vegetarian = true },
+                new() { PizzaID = 4, Name = "Pizza 4", Description = "Fourth Pizza in List", Price = 4.99M, Vegan = true, Vegetarian = true }
+            } );
+        }
     }
 }
