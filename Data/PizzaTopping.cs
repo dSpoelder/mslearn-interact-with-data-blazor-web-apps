52c397554e8371d07d3d2ec6e7346f7466fea772
Daniël Spoelder <daniel.spoelder@vanhalteren.com>
2023-12-18 10:49:06 +0100

    new file:   Data/PizzaTopping.cs 	modified:   Pages/Index.razor 	new file:   Pages/SharePage.razor 	new file:   Services/OrderState.cs 	new file:   Shared/ConfigPizzaDialog.razor

---
+++ Data/PizzaTopping.cs
@@ -0,0 +1,8 @@
+namespace BlazingPizza.Data
+{
+    public class PizzaTopping
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
---
+++ Services/OrderState.cs
@@ -0,0 +1,41 @@
+using BlazingPizza.Shared;
+
+namespace BlazingPizza.Services
+{
+    public class OrderState
+    {
+        public bool ShowingConfigDialog { get; private set; }
+        public Pizza ConfigPizza { get; private set; }
+        public Order Order { get; private set; } = new();
+
+        public void ShowPizzaConfigDialog(PizzaSpecial special)
+        {
+            // Copy Configuration Pizza from Selected Special, Flag Dialog Window as Visible
+            ConfigPizza = new()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+
+            ShowingConfigDialog = true;
+        }
+
+        public void ClosePizzaConfigDialog()
+        {
+            // Reset Data on Selected Pizza, Flag Dialog as Hidden
+            ConfigPizza = null;
+            ShowingConfigDialog = false;
+        }
+
+        public void ConfirmPizzaConfigDialog()
+        {
+            // Add Current Pizza Data to Order List, Execute Dialog Close Method
+            Order.Pizzas.Add(ConfigPizza);            
+            ClosePizzaConfigDialog();
+        }
+    }
+
+    
+}
